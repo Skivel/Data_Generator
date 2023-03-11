b2eacbbcc54e9c05791cbc236db5374ed558e16e
Skivel <denisbuhrov@gmail.com>
2023-03-11 17:43:32 +0100

    Now user can update table ✅ but form to update fields have bugs... try fix it later✨

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Schemas
 
 
-@admin.register(Schemas)
-class ModelsAdmin(admin.ModelAdmin):
-    list_display = ['user_name', 'title']
+class SchemasAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user_name', 'data_create', 'data_update')
+    list_filter = ('user_name', 'data_create', 'data_update')
+    search_fields = ('title', 'user_name')
+    ordering = ('-data_create',)
+
+admin.site.register(Schemas, SchemasAdmin)
